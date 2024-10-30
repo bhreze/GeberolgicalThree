c725d07ed70241b2d3a688028afb12697f313ef3
Даниил Сало <danill.salo@yandex.ru>
2024-10-30 14:56:20 +0500

    Добавьте файлы проекта.

---
+++ FamilyMembers.cs
@@ -0,0 +1,43 @@
+﻿public enum Gender { female, male}
+public class FamilyMembers
+{
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
+    public  string FullName { get; set; }
+    public FamilyMembers ?Mother { get; set; }
+    public FamilyMembers Father { get; set; }
+    public FamilyMembers? Wife { get; set; }
+    public FamilyMembers? Husband { get; set; }
+
+    public FamilyMembers[] GetGrandMother()
+    {
+        return new FamilyMembers[] { Mother?.Mother, Father?.Mother };
+    }
+
+
+    public FamilyMembers[] GetGrandFather()
+    {
+        return new FamilyMembers[] { Father?.Father, Father?.Mother };
+    }
+    public FamilyMembers GetSpouse()
+    {
+        if (Gender == Gender.female)
+            return Wife;
+        else
+            return Husband;
+    }
+    public FamilyMembers GetParent()
+    {
+        if (Gender == Gender.male)
+            return Mother;
+        else
+            return Father;
+    }
+    public FamilyMembers GetChild()
+    {
+        if (Gender == Gender.male)
+            return Wife;
+        else
+            return Husband;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С_seminars
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            FamilyMembers GrandFatherOne = new FamilyMembers()
+            {
+                FullName = "Salo Nikolay",
+                Age = 68,
+                Gender = Gender.male
+            };
+            FamilyMembers GrandFatherSecond = new FamilyMembers()
+            {
+                FullName = "Viznuk Vladimir",
+                Age = 65,
+                Gender = Gender.male
+            };
+            FamilyMembers GrandMotherOne = new FamilyMembers()
+            {
+                FullName = "Salo Elizaveta",
+                Age = 64,
+                Gender = Gender.female
+            };
+            FamilyMembers GrandMotherSecond = new FamilyMembers()
+            {
+                FullName = "Viznuk Tatiana",
+                Age = 62,
+                Gender = Gender.female
+            };
+            
+            FamilyMembers Mother = new FamilyMembers()
+            {
+                FullName = "Salo Elena",
+                Age = 47,
+                Gender = Gender.female,
+                Father = GrandFatherSecond,
+                Mother = GrandMotherSecond
+            };
+            FamilyMembers Father = new FamilyMembers()
+            {
+                FullName = "Salo Maksim",
+                Age = 50,
+                Gender = Gender.male,
+                Father = GrandFatherOne,
+                Mother = GrandMotherOne
+            };
+            FamilyMembers Son = new FamilyMembers()
+            {
+                FullName = "Salo Daniil",
+                Age = 23,
+                Gender = Gender.male,
+                Mother = Mother,
+                Father = Father
+            };
+
+             var GrandMothers = Son.GetGrandMother();
+
+
+            Console.WriteLine($"Мать {Mother.FullName} для {Son.FullName}");
+            Console.WriteLine($"Отец {Father.FullName} для {Son.FullName}");
+
+        }
+    }
+}
